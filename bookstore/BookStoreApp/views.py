fd61754c5fa6df2a8b4ac822aa2a15056883f5f3
Daniel Ruvcheski <famruvcheski@gmail.com>
2024-07-01 02:22:36 +0200

    Index page finished

--- bookstore/BookStoreApp/views.py
+++ bookstore/BookStoreApp/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 def index(request):
-    return render(request, 'index.html')
+
+    queryset = Book.objects.filter().all()
+    context = {'books' : queryset}
+
+    return render(request, 'index.html', context = context)
+
+
+def book_detail(request, id):
+    return render(request, 'book_detail.html', context={'book': Book.objects.get(id=id)})
--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -1,15 +1,18 @@
 
 from django.contrib import admin
 from django.urls import path
-from BookStoreApp.views import index
+from BookStoreApp.views import index, book_detail
 from django.conf import settings
 
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+app_name = 'book'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index, name='index'),
+    path('suplement/<id>/', book_detail, name='book_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
